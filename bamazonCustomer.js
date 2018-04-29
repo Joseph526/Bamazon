7e696e85b12a5f5b7108f668c8ad9a7417f4efe9
Joseph526 <33866177+Joseph526@users.noreply.github.com>
2018-04-29 16:51:59 -0400

    DB query and basic display working

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -9,13 +9,31 @@ var connection = mysql.createConnection({
     user: process.env.MYSQL_USER,
     password: process.env.MYSQL_PW,
     database: process.env.MYSQL_DB
-  });
-  
-  // Initiate MySQL Connection.
-  connection.connect(function(err) {
+});
+
+// Initiate MySQL Connection.
+connection.connect(function(err) {
     if (err) {
-      console.error("error connecting: " + err.stack);
-      return;
+        console.error("error connecting: " + err.stack);
+        return;
     }
     console.log("connected as id " + connection.threadId);
-  });
+    bamazon.start();
+});
+
+// Declare functions
+var bamazon = {
+    start: function() {
+        // Goto display
+        bamazon.display();
+    },
+    // Display inventory of items
+    display: function() {
+        var query = "SELECT * FROM products";
+        connection.query(query, function(err, result) {
+            for (var i = 0; i < result.length; i++) {
+                console.log(result[i]);
+            }
+        });
+    }
+};
