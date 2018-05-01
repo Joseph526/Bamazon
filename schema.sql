bbe256681d484901b405d1b68fcb882197914da8
Joseph526 <33866177+Joseph526@users.noreply.github.com>
2018-04-30 21:58:42 -0400

    Added more items to DB

--- schema.sql
+++ schema.sql
@@ -13,6 +13,11 @@ CREATE TABLE products (
 
 INSERT INTO products (product_name, department_name, price, stock_quantity) VALUES ("baseball", "sporting_goods", 2.50, 100);
 INSERT INTO products (product_name, department_name, price, stock_quantity) VALUES ("football", "sporting_goods", 4.00, 70);
-INSERT INTO products (product_name, department_name, price, stock_quantity) VALUES ("barbecue grill", "home_garden", 100, 20);
-INSERT INTO products (product_name, department_name, price, stock_quantity) VALUES ("lawn chair", "home_garden", 30, 55);
+INSERT INTO products (product_name, department_name, price, stock_quantity) VALUES ("barbecue grill", "home_garden", 100.00, 20);
+INSERT INTO products (product_name, department_name, price, stock_quantity) VALUES ("lawn chair", "home_garden", 30.00, 55);
 INSERT INTO products (product_name, department_name, price, stock_quantity) VALUES ("t-shirt", "men_clothing", 1.50, 225);
+INSERT INTO products (product_name, department_name, price, stock_quantity) VALUES ("gym shorts", "men_clothing", 3.00, 155);
+INSERT INTO products (product_name, department_name, price, stock_quantity) VALUES ("socks", "men_clothing", 0.50, 500);
+INSERT INTO products (product_name, department_name, price, stock_quantity) VALUES ("t-shirt", "women_clothing", 1.25, 350);
+INSERT INTO products (product_name, department_name, price, stock_quantity) VALUES ("sneakers", "women_clothing", 50.00, 30);
+INSERT INTO products (product_name, department_name, price, stock_quantity) VALUES ("iPhone case", "electronics", 4.99, 225);
